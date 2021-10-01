ba6cc40ff3d5a6964d43713dbbb265e23a48e43e
Hendi Santika <hendisantika@gmail.com>
2021-10-01 19:27:14 +0700

    #12 - Add User Repository

---
+++ src/main/java/com/hendisantika/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-custom-ID-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/10/21
+ * Time: 19.26
+ */
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
